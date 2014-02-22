68e937b0ce3ef30b96cc151e9a60c639da5a1093
moyashidaisuke <guitaristdaisuke@gmail.com>
2014-02-22 17:38:47 +0900

    コーテーションがまざってたので修正

--- mixi.rb
+++ mixi.rb
@@ -1,45 +1,45 @@
 # -- coding: utf-8
-require "rubygems"
-require "mechanize"
-require "kconv"
+require 'rubygems'
+require 'mechanize'
+require 'kconv'
 
 # setting
-DOMAIN = "http://mixi.jp"
+DOMAIN = 'http://mixi.jp'
 MAIL = ARGV[0]
 PASS = ARGV[1]
 COMMUNITY_ID = ARGV[2]
 
 if MAIL == nil || PASS == nil || COMMUNITY_ID == nil then
-  puts "3ARGS is require"
+  puts '3ARGS is require'
   exit 1
 end
 
 agent = Mechanize.new
-agent.user_agent_alias = "Mac Safari"
+agent.user_agent_alias = 'Mac Safari'
 
 # ログイン
 agent.get(DOMAIN)
 p 'login'
-agent.page.form("login_form").field("email").value=MAIL
-agent.page.form("login_form").field("password").value=PASS
-agent.page.form("login_form").submit
+agent.page.form('login_form').field('email').value=MAIL
+agent.page.form('login_form').field('password').value=PASS
+agent.page.form('login_form').submit
 
 # トップページ
-agent.get(DOMAIN + "/home.pl")
+agent.get(DOMAIN + '/home.pl')
 p 'top'
 
 # コミュニティのトピックページ
 agent.get(DOMAIN + '/list_bbs.pl?id=' + COMMUNITY_ID)
 p 'community'
 
-agent.page.search("div.pageTitle/h2").each do |div|
-  p "commubnity title : " + div.inner_text.gsub("\n", "")
+agent.page.search('div.pageTitle/h2').each do |div|
+  p 'commubnity title : ' + div.inner_text.gsub('\n', '¥n')
 end
 
 # 最新の更新日時
-div =agent.page.search("dl.bbsList01//span.date")[0]
-now_updated = div.inner_text.gsub("\n", "")
-p  "now_updated : " + now_updated
+div =agent.page.search('dl.bbsList01//span.date')[0]
+now_updated = div.inner_text.gsub('\n', '')
+p  'now_updated : ' + now_updated
 
 
 # 前回の結果
