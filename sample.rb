a80d507d33bad83f88d70936bca4382f034e4eb1
moyashidaisuke <guitaristdaisuke@gmail.com>
2014-02-22 16:04:52 +0900

    http://morizyun.github.io/blog/ruby-nokogiri-scraping-tutorial/を参考にsample作成

--- sample.rb
+++ sample.rb
@@ -16,3 +16,19 @@ doc = Nokogiri::HTML.parse(html, nil, charset)
 
 # タイトルを表示
 p doc.title
+
+
+
+require 'rubygems'
+require 'mechanize'
+require 'kconv'
+
+agent = Mechanize.new
+agent.get('http://www.google.co.jp/')
+agent.page.form_with(:name => 'f'){|form|
+  form.field_with(:name => 'q').value = 'Ruby'
+  form.click_button
+}
+agent.page.link_with(:text => "オブジェクト指向スクリプト言語 Ruby".toutf8).click
+puts agent.page.uri
+puts agent.page.at('div#logo/img')['alt']
